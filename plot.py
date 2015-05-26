871c1c25ee79b0029f3bf53f12f3e7f6c940314c
David Knaack <david@knaack.me>
2015-05-26 20:54:49 +0200

    update

--- plot.py
+++ plot.py
@@ -6,30 +6,38 @@ import seaborn as sns
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
-locale.setlocale(locale.LC_ALL, "de_de")
+locale.setlocale(locale.LC_ALL, "de_DE")
 mpl.rcParams['axes.formatter.use_locale'] = True
 sns.set_context("paper")
 
+for fileName in glob.iglob('*/*.csv'):
+	print("Plotting " + fileName + "…")
 
-files = [fn for fn in glob.iglob(
-    '*/*.csv') if not os.path.basename(fn).startswith('dist')]
+	data = pd.io.parsers.read_csv(
+		fileName, names=["x", "y"], header=None)
+	save_path = os.getcwd() + "/" + fileName.replace(".csv", "") + '.png'
 
-for fileName in files:
-    print("Plotting " + fileName + "…")
-    data = pd.io.parsers.read_csv(
-        fileName, names=["x", "y"], header=None)
-    savePath = os.getcwd() + "/" + fileName.replace(".csv", "") + '.png'
+	fig = plt.figure()
+	ax = fig.add_subplot(111)
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
+	alpha = 0.1
+	if fileName.endswith("IS.csv"):
+		alpha = 0.5
 
-    ax.scatter(x=data["x"], y=data["y"], s=1)
+	ax.scatter(x=data["x"], y=data["y"], s=1, lw = 0, alpha=alpha)
 
-    ax.set_xlabel("Länge")
-    ax.set_ylabel("Zeit in s")
+	if os.path.basename(fileName).startswith('dist'):
+		ax.set_xlabel("Wiederholungen")
+		ax.set_ylabel("Zeit in s")
 
-    ax.set_xlim(0, 100000)
-    ax.set_ylim(0)
+		ax.set_xlim(0, 10000)
+		ax.set_ylim(0)
+	else:
+		ax.set_xlabel("Länge")
+		ax.set_ylabel("Zeit in s")
 
-    fig.savefig(savePath, bbox_inches='tight', dpi=300)
-    plt.close(fig)
+		ax.set_xlim(0, 100000)
+		ax.set_ylim(0)
+
+	fig.savefig(save_path, bbox_inches="tight", dpi=300)
+	plt.close(fig)
